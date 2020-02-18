ac20182c10536a45f9e35110f7542c25e4509fe0
Satyaki Ghosh <satyakig901@hotmail.com>
2020-02-18 15:03:06 -0700

    Fixing build file

--- worker/generateAssets.js
+++ worker/generateAssets.js
@@ -7,7 +7,7 @@ cp('-R', 'node_modules/', 'build/');
 
 
 const runtime = 'runtime: nodejs12\n';
-const instance = 'instance_class: F2\n';
+const instance = 'instance_class: F1\n';
 const service = `service: ${process.env.SERVICE_NAME || 'default'}`;
 
 sync(
