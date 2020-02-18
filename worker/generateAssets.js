6371e98234d361f2d054bada5669ec4716473c2a
Satyaki Ghosh <satyakig901@hotmail.com>
2020-02-18 13:55:52 -0700

    FOOD-42 fixing up package

---
+++ worker/generateAssets.js
@@ -0,0 +1,21 @@
+const cp = require('shelljs').cp;
+const sync = require('write').sync;
+
+cp('package.json', 'build/');
+cp('package-lock.json', 'build/');
+cp('-R', 'node_modules/', 'build/');
+
+
+const runtime = 'runtime: nodejs12\n';
+const instance = 'instance_class: F2\n';
+const service = `service: ${process.env.SERVICE}`;
+
+sync(
+  'build/app.yaml',
+  `${runtime}${instance}${service}\n`,
+  {
+    newline: true,
+    increment: false,
+    overwrite: true,
+  },
+);
