3ac5eabb9764bbe74649a9579b2df9d9f74374e0
danielnwaroh <daniel.nwaroh@ucalgary.ca>
2020-02-18 17:04:40 -0700

    react(client) stuff

--- database.bash
+++ database.bash
@@ -1,82 +1,28 @@
 #!/usr/bin/env bash
-
-curl -v -XPOST -H "Content-type: application/json" -d '{
-  "kind": "compute#instance",
-  "name": "db-3",
-  "zone": "projects/collaborative-teaching/zones/us-central1-a",
-  "machineType": "projects/collaborative-teaching/zones/us-central1-a/machineTypes/n1-standard-1",
-  "displayDevice": {
-    "enableDisplay": false
-  },
-  "metadata": {
-    "kind": "compute#metadata",
-    "items": []
-  },
-  "tags": {
-    "items": [
-      "http-server",
-      "https-server"
-    ]
-  },
-  "disks": [
-    {
-      "kind": "compute#attachedDisk",
-      "type": "PERSISTENT",
-      "boot": true,
-      "mode": "READ_WRITE",
-      "autoDelete": true,
-      "deviceName": "db-3",
-      "initializeParams": {
-        "sourceImage": "projects/debian-cloud/global/images/debian-9-stretch-v20200210",
-        "diskType": "projects/collaborative-teaching/zones/us-central1-a/diskTypes/pd-standard",
-        "diskSizeGb": "10"
-      },
-      "diskEncryptionKey": {}
-    }
-  ],
-  "canIpForward": false,
-  "networkInterfaces": [
-    {
-      "kind": "compute#networkInterface",
-      "subnetwork": "projects/collaborative-teaching/regions/us-central1/subnetworks/default",
-      "accessConfigs": [
-        {
-          "kind": "compute#accessConfig",
-          "name": "External NAT",
-          "type": "ONE_TO_ONE_NAT",
-          "networkTier": "PREMIUM"
-        }
-      ],
-      "aliasIpRanges": []
-    }
-  ],
-  "description": "",
-  "labels": {},
-  "scheduling": {
-    "preemptible": false,
-    "onHostMaintenance": "MIGRATE",
-    "automaticRestart": true,
-    "nodeAffinities": []
-  },
-  "deletionProtection": false,
-  "reservationAffinity": {
-    "consumeReservationType": "ANY_RESERVATION"
-  },
-  "serviceAccounts": [
-    {
-      "email": "165250393917-compute@developer.gserviceaccount.com",
-      "scopes": [
-        "https://www.googleapis.com/auth/cloud-platform"
-      ]
-    }
-  ]
-}' 'https://www.googleapis.com/compute/v1/projects/collaborative-teaching/zones/us-central1-a/instances'
-
-
-
-
-gcloud compute --project $PROJECT_ID ssh --zone us-central1-a db1
 apt-get update && apt-get upgrade -y
+apt install -y curl gnupg gnupg1 gnupg2
+
+echo -e "Add key"
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6A030B21BA07F4FB
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6A030B21BA07F4FB
+apt-get update && apt-get upgrade -y
+echo "deb http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+echo -e "Install GCloud"
+apt-get update && apt-get install -y google-cloud-sdk
+
+echo -e "Make Instance\n"
+gcloud beta compute --project=collaborative-teaching instances create db-1 --zone=us-central1-a --machine-type=f1-micro --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=165250393917-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=https-server --image=ubuntu-minimal-1910-eoan-v20200129 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=db-1 --reservation-affinity=any
+
+echo -e "Update Firewall\n"
+gcloud compute --project=collaborative-teaching firewall-rules create default-allow-https --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:443 --source-ranges=0.0.0.0/0 --target-tags=https-server
+
+
+echo -e "Install MongoDB\n"
+gcloud compute --project $PROJECT_ID ssh --zone us-central1-a db-1
+
 apt-get -y install mongodb
 service mongodb stop
 mkdir $HOME/db
