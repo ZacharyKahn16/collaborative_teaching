946da49ab99f61ad316d89e10328bd055b65a1d8
garlandkhuu <32853310+garlandkhuu@users.noreply.github.com>
2020-02-19 12:26:16 -0700

    Implemented master url http request with round robin load balancing using dummy data

--- load_balancer/src/app.ts
+++ load_balancer/src/app.ts
@@ -1,29 +1,38 @@
+/* tslint:disable */
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { createServer } from 'http';
-import io from 'socket.io';
+
+//Change the mock data to real list of masters queried from the database
+import { mockData } from './mockServerData';
 
 const app = express();
 app.use(cors());
 app.use(morgan('short'));
 
 const server = createServer(app);
-const ioServer = io(server);
 
-app.get('/', function(req, res) {
-  res.sendFile(`${__dirname}/index.html`);
+server.on('connection', (socket) => {
+  console.log('New conneection...');
 });
 
-ioServer.on('connection', function(socket) {
-  console.log('a user connected');
-
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-  });
-});
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.debug(`Server started at http://localhost:${PORT}`);
 });
+
+//TODO: Change this to real master list
+const masterList: any = mockData
+
+const getNextMaster = (): any => {
+  const nextMaster = masterList.shift();
+  masterList.push(nextMaster);
+  
+  return nextMaster;
+};
+
+app.get('/', function(req, res) {
+  res.send(getNextMaster());
+});
+
---
+++ load_balancer/src/mockServerData.ts
@@ -0,0 +1,8 @@
+const mockData: any = [
+  { masterUrl: "http://localhost:3001/"},
+  { masterUrl: "http://localhost:3002/"},
+  { masterUrl: "http://localhost:3003/"},
+  { masterUrl: "http://localhost:3004/"},
+];
+
+export { mockData };
