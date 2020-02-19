eaac6d04e481af108b09990bca469b0a45167a88
danielnwaroh <daniel.nwaroh@ucalgary.ca>
2020-02-19 12:44:14 -0700

    cleint stuff

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SyncingEditor } from "./SyncingEditor";
+
+const App = () => {
+  return (
+    <div>
+      <SyncingEditor />
+    </div>
+  );
+};
+
+export default App;
