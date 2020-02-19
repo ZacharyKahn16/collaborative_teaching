fa5c60e4a94c72ae1f9dadefe79f9414c4c53e9e
Satyaki Ghosh <satyakig901@hotmail.com>
2020-02-19 11:29:33 -0700

    Adding react build steps and storage code

--- load_balancer/generateAssets.js
+++ load_balancer/generateAssets.js
@@ -5,16 +5,15 @@ cp('package.json', 'build/');
 cp('package-lock.json', 'build/');
 cp('-R', 'node_modules/', 'build/');
 
-
 const runtime = 'runtime: nodejs12\n';
-const instance = 'instance_class: F1\n';
+const instance = 'instance_class: B1\n';
 const service = `service: ${process.env.SERVICE_NAME || 'default'}`;
+const scaling = 'basic_scaling:\n';
+const max_instance = '  max_instances: 1\n';
+const timeout = '  idle_timeout: 10m\n';
 
-sync(
-  'build/app.yaml',
-  `${runtime}${instance}${service}\n`, {
-    newline: true,
-    increment: false,
-    overwrite: true,
-  },
-);
+sync('build/app.yaml', `${runtime}${instance}${service}${scaling}${max_instance}${timeout}\n`, {
+  newline: true,
+  increment: false,
+  overwrite: true,
+});
