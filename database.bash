5ffb7a85990113295c9b3f95a42bc1de3b5774c9
Satyaki Ghosh <satyakig901@hotmail.com>
2020-02-18 17:27:56 -0700

    test

--- database.bash
+++ database.bash
@@ -2,20 +2,21 @@
 apt-get update && apt-get upgrade -y
 apt install -y curl
 
+echo -e "Add key"
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6A030B21BA07F4FB
-# Add the Cloud SDK distribution URI as a package source
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6A030B21BA07F4FB
+
+apt-get update && apt-get upgrade -y
 echo "deb http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-# Import the Google Cloud Platform public key
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-# Update the package list and install the Cloud SDK
+
+echo -e "Install GCloud"
 apt-get update && apt-get install -y google-cloud-sdk
 
-
+echo -e "Make Instance"
 gcloud beta compute --project=collaborative-teaching instances create db-1 --zone=us-central1-a --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=165250393917-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --image=debian-9-stretch-v20200210 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=db-1 --reservation-affinity=any
-
 gcloud compute --project=collaborative-teaching firewall-rules create default-allow-https --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:443 --source-ranges=0.0.0.0/0 --target-tags=https-server
-
 gcloud compute --project $PROJECT_ID ssh --zone us-central1-a db-1
 
 apt-get -y install mongodb
