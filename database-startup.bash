0006db1c88e2c7c69b4f89dc328ab2189d841aa1
Satyaki Ghosh <satyakig901@hotmail.com>
2020-02-20 12:55:54 -0700

    Updating build file

--- database-startup.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update && apt-get upgrade -y
-apt install -y curl gnupg2
-apt-get -y install mongodb
-service mongodb stop
-mkdir $HOME/db
-mongod --dbpath $HOME/db --port 80 --fork --logpath /var/tmp/mongodb
-exit
---
+++ scripts/nodejs-startup.bash
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+PROJECT_ID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+NAME=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/name" -H "Metadata-Flavor: Google")
+
+# Install dependencies
+apt-get update && apt-get upgrade -y
+apt-get install -yq ca-certificates build-essential supervisor curl gnupg2 nano
+mkdir /opt/nodejs
+curl https://nodejs.org/dist/v12.16.1/node-v12.16.1-linux-x64.tar.gz | tar xvzf - -C /opt/nodejs --strip-components=1
+ln -s /opt/nodejs/bin/node /usr/bin/node
+ln -s /opt/nodejs/bin/npm /usr/bin/npm
+
+# Get Code
+gsutil cp -r gs://collaborative-teaching.appspot.com/load_balancer /opt
+
+# Install Dependencies
+cd /opt/load_balancer
+npm ci
+npm run build
+cp package.json build/
+cp package-lock.json build/
+cp -r node_modules/ build/
+
+# Create a nodeapp user. The application will run as this user.
+useradd -m -d /home/nodeapp nodeapp
+chown -R nodeapp:nodeapp /opt/load_balancer
+
+# Configure supervisor to run the node app.
+cat >/etc/supervisor/conf.d/node-app.conf << EOF
+[program:nodeapp]
+directory=/opt/load_balancer/build
+command=npm start
+autostart=true
+autorestart=true
+user=nodeapp
+environment=HOME="/home/nodeapp",USER="nodeapp",NODE_ENV="production",PROJECT_ID=${PROJECT_ID},NAME=${NAME}
+stdout_logfile=syslog
+stderr_logfile=syslog
+EOF
+
+supervisorctl reread
+supervisorctl update
+
+# Application should now be running under supervisor
