8290e058dffa4ea592ef214cd386148d86422c6b
jamesperalta <jamesperalta35@gmail.com>
2020-02-22 16:08:57 -0700

    Added code to run mongodb on start

--- master/src/app.ts
+++ master/src/app.ts
@@ -1,17 +1,18 @@
+// Creates a express application
 import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-
 const app = express();
-
-app.use(cors());
-app.use(morgan('short'));
-
+// Creates an http server using express
 import { createServer } from 'http';
 const server = createServer(app);
-import io from 'socket.io';
 
+// Creates a web socket on this server
+import io from 'socket.io';
 const ioServer = io(server);
+
+import cors from 'cors';
+import morgan from 'morgan';
+app.use(cors());
+app.use(morgan('short'));
 
 app.get('/', function(req, res) {
   res.sendFile(`${__dirname}/index.html`);
--- scripts/startup-database.bash
+++ scripts/startup-database.bash
@@ -4,14 +4,11 @@ PROJECT_ID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project
 NAME=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/name" -H "Metadata-Flavor: Google")
 
 # Install dependencies
-apt-get update && apt-get upgrade -y
-apt-get install -yq ca-certificates build-essential curl gnupg2 nano vim net-tools
+apt-get update
 apt-get install -y mongodb
 
-systemctl status mongodb
-sed -i '/bind_ip = 127.0.0.1/c\bind_ip = 0.0.0.0' /etc/mongodb.conf
-sed -i '/#port = 27017/c\port = 80' /etc/mongodb.conf
-systemctl restart mongodb
-mongo --eval 'db.runCommand({ connectionStatus: 1, showPrivileges: true})'
+service mongodb stop
+mkdir $HOME/db
+mongod --dbpath $HOME/db --port 80 --bind_ip 0.0.0.0 --fork --logpath /var/tmp/mongodb
 
 gcloud compute --project=${PROJECT_ID} instances add-metadata ${NAME} --metadata startup-status=running,startup-on=$(date +%s) --zone=us-central1-a
