086c4f450c24675775660b139fdb96cb7d3c8752
garlandkhuu <garlandkhuu123@gmail.com>
2020-03-17 14:53:06 -0600

    Formatted file list

--- client/src/FileList.jsx
+++ client/src/FileList.jsx
@@ -4,20 +4,23 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 import React from "react";
 import PropTypes from "prop-types";
+import "./Styles/FileList.css";
 // import { withStyles } from "@material-ui/core/styles";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(fileName, courseName, owner, dateUploaded) {
+  return { fileName, courseName, owner, dateUploaded };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9)
+  createData("Replication & Fault Tolerance.ppt", "CPSC 559", "Garland Khuu", "02-21-2020"),
+  createData("Dynamic Programming.pdf", "CPSC 413", "Garland Khuu", "11-16-2019"),
+  createData("Utilitarianism.pdf", "PHIL 249", "Garland Khuu", "09-20-2016"),
+  createData("10 Tips for Success in Computer Science.ppt", "", "Garland Khuu", "03-05-2020"),
+  createData("Password Hashing.pdf", "CPSC 329", "Garland Khuu", "01-28-2019")
 ];
 
 const FileList = props => {
@@ -25,24 +28,34 @@ const FileList = props => {
     <TableContainer>
       <Table aria-label="simple table">
         <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          <TableRow className="bold">
+            <TableCell className="bold" align="left">File Name</TableCell>
+            <TableCell className="bold" align="left">Course</TableCell>
+            <TableCell className="bold" align="left">Owner</TableCell>
+            <TableCell className="bold" align="left">Date Uploaded</TableCell>
+            <TableCell className="bold" align="left">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
+            <TableRow key={row.fileName}>
+              <TableCell align="left">
+                <Typography variant="body2">
+                  <Link
+                      color="primary"
+                      href="#"
+                      onClick={() => {
+                      }}
+                  >
+                    {row.fileName}
+                  </Link>
+                </Typography>
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="left">{row.courseName ? row.courseName : "None"}</TableCell>
+              <TableCell align="left">{row.owner}</TableCell>
+              <TableCell align="left">{row.dateUploaded}</TableCell>
+              <TableCell align="left">Actions</TableCell>
+
             </TableRow>
           ))}
         </TableBody>
