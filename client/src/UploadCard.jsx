9f6d3600a28f8fb581fcfe4554e8f70778dba375
garlandkhuu <garlandkhuu123@gmail.com>
2020-03-21 16:34:34 -0600

    remove unused imports

--- client/src/UploadCard.jsx
+++ client/src/UploadCard.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import PropTypes from "prop-types";
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from '@material-ui/core/Button';
-
 import "./Styles/Card.css";
-
-import {withStyles} from "@material-ui/core/styles";
 
 class UploadCard extends Component {
     constructor(props) {
