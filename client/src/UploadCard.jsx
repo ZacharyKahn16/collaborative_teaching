cab37d0c8850184edfafbe6dc71796648020cb78
garlandkhuu <garlandkhuu123@gmail.com>
2020-03-21 16:29:57 -0600

    File upload setup

--- client/src/UploadCard.jsx
+++ client/src/UploadCard.jsx
@@ -15,7 +15,30 @@ class UploadCard extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            fileDisplayName: "",
+            uploadedFile: null
+        };
+
+        this.onBrowseChange = this.onBrowseChange.bind(this);
+        this.cancelUpload = this.cancelUpload.bind(this);
+    }
+
+    onBrowseChange = (e) => {
+        let files = e.target.files;
+
+        this.setState(() => ({
+            fileDisplayName: files[0].name,
+            uploadedFile: files[0]
+        }));
+    };
+
+    cancelUpload = () => {
+        this.props.closeModal();
+        this.setState(() => ({
+            fileDisplayName: "",
+            uploadedFile: null
+        }));
     }
 
     render() {
@@ -31,22 +54,27 @@ class UploadCard extends Component {
                   <Grid className="upload-box-container" container spacing={0}>
                       <Grid className="upload-box-left" item xs={9}>
                           <Typography className="file-name-p" variant="body2" component="p" noWrap>
-
+                              {this.state.fileDisplayName}
                           </Typography>
                       </Grid>
                       <Grid className="upload-box-right" item xs={3}>
                           <Button className="browse-btn" component="label" variant="contained" color="primary">
-                              <input className="display-none" type="file"/>
+                              <input className="display-none" type="file" onChange={this.onBrowseChange}/>
                               Browse
                           </Button>
                       </Grid>
                   </Grid>
                   <br/>
                   <br/>
-                  <Button className="mr-1" variant="contained" color="primary">
+                  <Button
+                      className="mr-1"
+                      variant="contained"
+                      color={this.state.uploadedFile ? "primary" : ""}
+                      disabled={!this.state.uploadedFile}
+                  >
                       Upload
                   </Button>
-                  <Button variant="contained" onClick={this.props.closeModal}>
+                  <Button variant="contained" onClick={this.cancelUpload}>
                       Cancel
                   </Button>
               </CardContent>
