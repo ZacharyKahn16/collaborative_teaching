71b75ebf05ca1e8fe71cb66dbad395c94ae4ad2e
Satyaki Ghosh <satyakig901@hotmail.com>
2020-03-26 21:52:47 -0600

    Fixing website script

--- scripts/startup-website.bash
+++ scripts/startup-website.bash
@@ -6,13 +6,15 @@ NAME=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/name
 apt-get update && apt-get upgrade -y
 apt-get install -yq ca-certificates build-essential supervisor curl gnupg2 nano vim net-tools nginx ufw
 
-sudo ufw allow 'Nginx HTTP'
-systemctl status nginx
+sudo ufw allow 'Nginx Full'
+sudo systemctl status nginx
 
 cd /var/www/html/
-gsutil cp -r gs://collaborative-teaching.appspot.com/client ./
-mv  -v ~/var/www/html/client/* /var/www/html/
+gsutil cp -r gs://collaborative-teaching.appspot.com/build ./
+mv  -v /var/www/html/build/* /var/www/html/
 sudo rm index.nginx-debian.html
+sudo rm -rf build/
 sudo systemctl restart nginx
+sudo systemctl status nginx
 
 gcloud compute --project=${PROJECT_ID} instances add-metadata ${NAME} --metadata startup-status=running,startup-on=$(date +%s) --zone=us-central1-a
