6d1cf13e228057c62061173838d4956d7e77fdfc
Satyaki Ghosh <satyakig901@hotmail.com>
2020-03-26 19:58:19 -0600

    Updating website script

--- scripts/startup-website.bash
+++ scripts/startup-website.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+PROJECT_ID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+NAME=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/name" -H "Metadata-Flavor: Google")
 
 # Install dependencies
 apt-get update && apt-get upgrade -y
@@ -10,6 +12,7 @@ systemctl status nginx
 cd /var/www/html/
 gsutil cp -r gs://collaborative-teaching.appspot.com/client ./
 mv  -v ~/var/www/html/client/* /var/www/html/
-sudo index.nginx-debian.html
+sudo rm index.nginx-debian.html
 sudo systemctl restart nginx
 
+gcloud compute --project=${PROJECT_ID} instances add-metadata ${NAME} --metadata startup-status=running,startup-on=$(date +%s) --zone=us-central1-a
